e7da05570cfec7566d213a2757ca49d8906b7b2c
LordWizardy <l.wizardy@gmail.com>
2024-02-19 19:36:40 +0100

    Initial

---
+++ Assets/BallScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 5;
+    private float flip = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = Vector2.right * speed * flip;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        flip *= -1;
+    }
+}
